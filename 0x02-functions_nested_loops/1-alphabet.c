7ccaacc67b81e6965959786922b2e657cc70f9ab
TKITO-D-Ahmed <ahmedtkitoemc2@gmail.com>
2023-08-15 06:29:44 -0700

    we wrote a function that prints the alphabet using the function print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Description: we will write  a function that prints the alphabet,
+ * in lowercase using only _putchar twine in the code
+ *
+ * Return: 0 Always
+*/
+
+void print_alphabet(void)
+{
+	for (int i = 'a' ; i < 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
+
