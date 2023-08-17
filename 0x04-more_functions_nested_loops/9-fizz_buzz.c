1c73dd0a73005f1c835bd92e9896b3071447adc6
TKITO-D-Ahmed <ahmedtkitoemc2@gmail.com>
2023-08-17 11:29:04 -0700

    FizzBuzz program

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,6 +14,7 @@ int main(void)
 	int i;
 
 	for (i = 1 ; i <= 100 ; i++)
+	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
@@ -22,5 +23,6 @@ int main(void)
 			printf("Buzz");
 		else
 			printf("%d", i);
+	}
 
 }
