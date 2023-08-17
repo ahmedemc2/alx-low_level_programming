4e3a5a5c048cf71b88f19ea320ef72dd2788c207
TKITO-D-Ahmed <ahmedtkitoemc2@gmail.com>
2023-08-17 06:00:33 -0700

    function that prints the numbers, from 0 to 9 expect 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - entry point
+ *
+ * Description: function that prints the numbers, from 0 to 9
+ * except 2 and 4
+ *
+ * Return : nothing
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		if (i != 50 && i!= 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
