8c432b0387edd030e0e92e334c8d5d3cd233f94c
TKITO-D-Ahmed <ahmedtkitoemc2@gmail.com>
2023-08-18 16:56:32 -0700

    program that finds and prints the largest prime factor of the number 612852475143

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 /**
- * main - entry point
+ * _sqrt - find the square root
  *
- * Description: program that finds and prints the largest prime factor 
+ * @x : input number
+ *
+ * Return: square root of 'x'
+*/
+
+double _sqrt(double x)
+{
+	float sqrt, tmp;
+
+	sqrt = x / 2;
+	tmp = 0;
+
+	while (sqrt != tmp)
+	{
+		tmp = sqrt;
+		sqrt = (x / tmp + tmp) / 2;
+	}
+	return (sqrt);
+}
+
+/**
+ * largest_prime_factor - finds and prints the largest
+ * prime factor of a number
+ *
+ * @num : number to find his largest prime factor
+*/
+
+void largest_prime_factor(long int num)
+{
+	int prmNu, largest;
+
+	while (num % 2 == 0)
+		num = num / 2;
+
+	for (prmNu = 3 ; prmNu <= _sqrt(num) ; prmNu += 2)
+	{
+		while (num % prmNu == 0)
+		{
+			num = num / prmNu;
+			largest = prmNu;
+		}
+	}
+
+	if (num > 2)
+		largest = num;
+	printf("%d\n", largest);
+}
+
+/**
+ * main - Entry point
+ *
+ * Description: program that finds and prints the largest prime factor
  * of the number 612852475143
  *
- * Return : 0 Always
+ * return: (0) Always
 */
 
 int main(void)
 {
-	unsigned long long n = 10;
-	bool getout = true;
-
-	while(i < n && getout == true)
-	{
-		if (n % i == 0)
-		{
-			getout = false;
-			printf("%llu is not a prime number", n);
-		}
-	}
+	largest_prime_factor(612852475143);
+	return (0);
 }
