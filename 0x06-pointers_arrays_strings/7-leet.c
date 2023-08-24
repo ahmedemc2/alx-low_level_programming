069f775b65eedee3c14f9f7a551c6f1cb2288f23
TKITO-D-Ahmed <ahmedtkitoemc2@gmail.com>
2023-08-24 08:01:08 -0700

    function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ *
+ * @str: the input string
+ *
+ * Return: the string encoded
+*/
+
+char *leet(char *str)
+{
+	char alphabet[] = "OLEAToleat";
+	char numbers[] = {'0', '1', '3', '4', '7', '0', '1', '3', '4', '7'};
+	int i;
+
+	while (*str)
+	{
+		for (i = 0 ; i <= 10 ; i++)
+		{
+			if (*str == alphabet[i])
+				*str = numbers[i];
+		}
+		str++;
+	}
+	return (str);
+}
