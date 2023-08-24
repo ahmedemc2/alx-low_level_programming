90856de3573d37b47fba30b6fa2a1146e72a236d
TKITO-D-Ahmed <ahmedtkitoemc2@gmail.com>
2023-08-24 06:56:54 -0700

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,67 +1,38 @@
 #include "main.h"
 
 /**
- * isLower - look if the alphabet is lowercase
- *
- * @c : the alphabet
- *
- * Return: number of the character in ascii code
-*/
-
-int isLower(char c)
-{
-	return (c >= 97 && c <= 122);
-}
-
-/**
- * toAvoid - function that look for the separators word to avoid
- *
- * @c : the char to check if is a separator
- *
- * Return: (1) if there is a separator and (0) if not
-*/
-
-int toAvoid (char c)
-{
-	int i;
-	char avoid[] = " \n,.;!?(){\"}";
-
-	for (i = 0 ; i < 13 ; i++)
-		if (c == avoid[i])
-			return (1);
-	return (0);
-}
-
-/**
  * cap_string - function that capitalizes all words of a string
  *
- * @s : the string
+ * @str: the string to be capitalized
  *
- * Return: the string capitalized
+ * Return: a pointer ro the changed string
 */
 
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int found = 1;
-	char *p = s;
+	int i = 0;
 
-	while (*s)
+	while (str[i])
 	{
-		if (*s == '\t')
-			*s = ' ';
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
 
-		if (toAvoid(*s))
-			found = 1;
-
-		else if (isLower(*s) && found)
-		{
-			*s -= 32;
-			found = 0;
-		}
-		else
-			found = 0;
-		s++;
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		i == 0)
+			str[i] -= 32;
+		i++;
 	}
-	return (p);
+	return (str);
 }
-
