ebc1f49bc4d15e9d502866e068ddd36dd2726eba
TKITO-D-Ahmed <ahmedtkitoemc2@gmail.com>
2023-09-05 23:55:21 -0700

    function that splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,6 +16,7 @@ int counter(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		if (str[i] != ' ')
 			nb++;
+	nb++;
 	return (nb);
 }
 
@@ -49,9 +50,7 @@ char **strtow(char *str)
 		{
 			for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 				;
-			j++;
 			ptr[wc] = (char *)malloc(j * sizeof(char));
-			j--;
 			if (ptr[wc] == NULL)
 			{
 				for (k = 0; k < wc; k++)
