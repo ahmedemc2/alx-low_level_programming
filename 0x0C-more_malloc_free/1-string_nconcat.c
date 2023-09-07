a888a499498d9a17c227e3012f0f1779896b7c21
TKITO-D-Ahmed <ahmedtkitoemc2@gmail.com>
2023-09-07 06:23:15 -0700

    function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -50,7 +50,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	}
 
-	for (i = 0; i < x - n; i++)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		*(concat + i) = *(s1 + i);
 	}
