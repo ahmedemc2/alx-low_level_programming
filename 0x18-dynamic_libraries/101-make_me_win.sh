82aa25194d5a24f9637903cceeee10bee56b439f
TKITO-D-Ahmed <ahmedtkitoemc2@gmail.com>
2023-11-13 15:59:23 -0800

    Please make me win!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/ahmedemc2/alx-low_level_programming/master/0x18-dynamic_libraries/libhacker.so
-export LD_PRELOAD="$PWD/../libhacker.so"
+wget -P .. https://raw.githubusercontent.com/ahmedemc2/alx-low_level_programming/master/0x18-dynamic_libraries/libhackers.so
+export LD_PRELOAD="$PWD/../libhackers.so"
